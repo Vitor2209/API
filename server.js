36af739e3ff6d6e1044d0d5cbeb3b4aa3d73666b
Vitor Dutra Melo <vitordutra1125@gmail.com>
2025-10-09 18:11:37 +0100

    .

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import express from 'express'
+import cors from 'cors'
 import { PrismaClient } from '@prisma/client'
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 const prisma = new PrismaClient()
+
+
 
 app.put('/usuarios/:id', async (req, res) => {
 
